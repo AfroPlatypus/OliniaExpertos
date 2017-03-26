5dd27d04a23422428fa219fd35f8ab656b20a85b
AfroMonkey <moisalejandro@gmail.com>
2017-03-25 18:09:16 -0600

    [REF]

--- app/src/main/java/com/afroplatypus/olinia/ChatActivity.java
+++ app/src/main/java/com/afroplatypus/olinia/ChatActivity.java
@@ -1,37 +1,22 @@
 package com.afroplatypus.olinia;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseListAdapter;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class ChatActivity extends AppCompatActivity {
 
+    public static final String MESSAGES_CHILD = "messages";
     // Firebase instance variables
     private DatabaseReference mFirebaseDatabaseReference;
     private FirebaseListAdapter<Message> mFirebaseAdapter;
-
-    public static class MessageViewHolder extends RecyclerView.ViewHolder {
-        public TextView messageTextView;
-        public TextView messengerTextView;
-
-        public MessageViewHolder(View v) {
-            super(v);
-            messageTextView = (TextView) itemView.findViewById(R.id.messageTextView);
-            messengerTextView = (TextView) itemView.findViewById(R.id.messengerTextView);
-        }
-    }
-
-    public static final String MESSAGES_CHILD = "messages";
-
     private ListView mMessageRecyclerView;
     private LinearLayoutManager mLinearLayoutManager;
 
