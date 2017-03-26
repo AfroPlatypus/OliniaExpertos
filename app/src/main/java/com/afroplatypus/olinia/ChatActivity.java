b6522a598c1a308d7595b6132837baaf5b77d27d
AfroMonkey <moisalejandro@gmail.com>
2017-03-25 19:05:09 -0600

    [BUG] doble app

--- app/src/main/java/com/afroplatypus/olinia/ChatActivity.java
+++ app/src/main/java/com/afroplatypus/olinia/ChatActivity.java
@@ -2,7 +2,6 @@ package com.afroplatypus.olinia;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -14,25 +13,23 @@ import com.google.firebase.database.FirebaseDatabase;
 public class ChatActivity extends AppCompatActivity {
 
     public static final String MESSAGES_CHILD = "messages";
-    // Firebase instance variables
+
     private DatabaseReference mFirebaseDatabaseReference;
     private FirebaseListAdapter<Message> mFirebaseAdapter;
     private ListView mMessageRecyclerView;
-    private LinearLayoutManager mLinearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        // Set default username is anonymous.
 
-        // Initialize ProgressBar and RecyclerView.
         mMessageRecyclerView = (ListView) findViewById(R.id.list);
-        mLinearLayoutManager = new LinearLayoutManager(this);
-        mLinearLayoutManager.setStackFromEnd(true);
-
-        // New child entries
         mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        getMessages();
+        mMessageRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    private void getMessages() {
         mFirebaseAdapter = new FirebaseListAdapter<Message>(this, Message.class, R.layout.message, mFirebaseDatabaseReference.child(MESSAGES_CHILD)) {
             @Override
             protected void populateView(View v, Message message, int position) {
@@ -46,7 +43,5 @@ public class ChatActivity extends AppCompatActivity {
                 }
             }
         };
-
-        mMessageRecyclerView.setAdapter(mFirebaseAdapter);
     }
 }
