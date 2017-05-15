908c5baaf4aa04b3b69d0aec1326c495bf1b6238
Omar Saucedo <omaroso27@gmail.com>
2017-05-14 20:12:56 -0500

    NPI

--- app/src/main/java/com/afroplatypus/olinia/ChatSelectionActivity.java
+++ app/src/main/java/com/afroplatypus/olinia/ChatSelectionActivity.java
@@ -1,13 +1,22 @@
 package com.afroplatypus.olinia;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ChatSelectionActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private Intent intentLogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +24,44 @@ public class ChatSelectionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat_selection);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mAuth = FirebaseAuth.getInstance();
+        intentLogIn = new Intent(this, LogInActivity.class);
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                if(user == null){
+                    startActivity(intentLogIn);
+                    ChatSelectionActivity.this.finish();
+                }
+            }
+        };
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                mAuth.signOut();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
 }
--- app/src/main/java/com/afroplatypus/olinia/LogInActivity.java
+++ app/src/main/java/com/afroplatypus/olinia/LogInActivity.java
@@ -2,28 +2,139 @@ package com.afroplatypus.olinia;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LogInActivity extends AppCompatActivity {
 
-    TextView anon;
-    Intent intent;
+    TextView anon, register;
+    EditText txtUserName, txtPass, txtConfirm;
+    Intent intentLoad;
+    Button btnLogIn;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        mAuth = FirebaseAuth.getInstance();
         anon = (TextView) findViewById(R.id.txtAnon);
-        intent = new Intent(this, LoadActivity.class);
+        register = (TextView) findViewById(R.id.txtRegister);
+        txtUserName = (EditText) findViewById(R.id.user_name);
+        txtPass = (EditText) findViewById(R.id.pass);
+        txtConfirm = (EditText) findViewById(R.id.confirm_pass);
+        btnLogIn = (Button) findViewById(R.id.btnLogIn);
+        intentLoad = new Intent(this, LoadActivity.class);
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    startActivity(intentLoad);
+                    LogInActivity.this.finish();
+                } else {
+                    // User is signed out
+                }
+            }
+        };
         anon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(intent);
-                LogInActivity.this.finish();
+                mAuth.signInAnonymously()
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful()){
+                                    Toast.makeText(LogInActivity.this, "Algo salió mal :( Vuelve a intentarlo más tarde.", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
             }
         });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(register.getText().toString().equals("Registrarme")) {
+                    btnLogIn.setText("Registrarme");
+                    txtConfirm.setVisibility(View.VISIBLE);
+                    register.setText("Iniciar sesión");
+                }
+                else {
+                    btnLogIn.setText("Iniciar sesión");
+                    txtConfirm.setVisibility(View.GONE);
+                    register.setText("Registrarme");
+                }
+            }
+        });
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail, pass;
+                mail = txtUserName.getText().toString().trim();
+                pass = txtPass.getText().toString();
+                if (mail.length() > 0 && pass.length() > 0) {
+                    if (isValidEmail(mail)) {
+                        if (btnLogIn.getText().toString().equals("Iniciar sesión")){
+                            mAuth.signInWithEmailAndPassword(mail, pass);
+                        } else {
+                            if (pass.equals(txtConfirm.getText().toString())){
+                                mAuth.createUserWithEmailAndPassword(mail, pass)
+                                        .addOnCompleteListener(LogInActivity.this, new OnCompleteListener<AuthResult>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                                if (!task.isSuccessful()){
+                                                    Toast.makeText(LogInActivity.this, "Algo salió mal :( Vuelve a intentarlo más tarde.", Toast.LENGTH_LONG).show();
+                                                }
+                                            }
+                                        });
+                            } else {
+                                Toast.makeText(LogInActivity.this, "Las contraseñas no coinciden.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    } else {
+                        Toast.makeText(LogInActivity.this, "Correo no valido.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    Toast.makeText(LogInActivity.this, "Falta llenar algunos campos.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private boolean isValidEmail(String email){
+        Pattern pat = Pattern.compile("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,4})+$");
+        Matcher mat = pat.matcher(email);
+        return mat.matches();
     }
 }
